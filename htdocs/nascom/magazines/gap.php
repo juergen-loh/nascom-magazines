ecc02430f680c18497660dde12026c5c0015e41b
juergen-loh <github@jloh.de>
2021-12-17 19:57:30 +0100

    Revised scorpio-news

--- htdocs/nascom/magazines/gap.php
+++ htdocs/nascom/magazines/gap.php
@@ -1,10 +1,10 @@
 <!-- gap.php -->
 	<tr>
-		<th class="chTitle">To&shy;pic</th>
-		<th class="chAuthor">Au&shy;thor</th>
+		<th class="chTitle">Topic</th>
+		<th class="chAuthor">Author</th>
 		<th class="chCategory">Ca&shy;te&shy;gory</th>
 		<th class="chMagazine">Ma&shy;ga&shy;zine</th>
 		<th class="chIssue">Is&shy;sue</th>
-		<th class="chPage">Pa&shy;ge</th>
+		<th class="chPage">Page</th>
 	</tr>
 <!-- /gap.php -->
